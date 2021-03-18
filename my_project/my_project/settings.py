501ebdf11ee54fce11a7438d3d9cd830bab46ea6
zhaojun <zhaojun@mifanli.com>
2021-03-18 21:04:54 +0800

    修改配置

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*94mng*x#ik(h-81(+fi9enhfq^2sog#(s&zw)vgsrurq)5m_s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }

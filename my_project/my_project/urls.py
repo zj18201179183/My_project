62e32839b58f6d3fd7a2e6e67609837cb4be64f5
zhaojun <zhaojun@mifanli.com>
2021-03-22 21:03:27 +0800

    删除静态文件

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
 ]
 
 # 开发环境配置
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     from django.conf.urls.static import static
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
